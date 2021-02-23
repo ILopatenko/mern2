4ad5db20cf8f12ec68145d4a156068ab321b3ae7
Iurii Lopatenko <yalopatenko@gmail.com>
2021-02-22 18:50:30 -0800

    Custom Auth Middleware & JWT Verify. 3.5

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,9 +4,23 @@ const express = require('express');
 //Create a router from express ROUTER
 const router = express.Router();
 
-//@description: Create a test route with ROUTER
+//Import middleware AUTH.js
+const auth = require('../../middleware/auth');
+
+//Import User model (schema)
+const User = require('../../models/User');
+
+//@description: Create a route for AUTHORIZATION
 //@route information: GET to api/auth
 //@access: PUBLIC
-router.get('/', (req, res) => res.send('AUTH route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server ERROR');
+  }
+});
 
 module.exports = router;
