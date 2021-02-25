4bc1aeffaa329487c772c6fbccc8156734ab74cb
Iurii Lopatenko <yalopatenko@gmail.com>
2021-02-24 20:49:32 -0800

    Connecting To MongoDB With Mongoose. 2.3

---
+++ config/db.js
@@ -0,0 +1,27 @@
+//Import MONGOOSE
+const mongoose = require("mongoose");
+
+//Import CONFIG
+const config = require("config");
+
+//Import mongoURI from default.json in folder config
+const db = config.get("mongoURI");
+
+//Create a connection
+const connectDB = async () => {
+  try {
+    //*TRY to connect
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    // If DB was connected - log a message
+    console.log("MongoDB was connected ...");
+  } catch (error) {
+    //If ERROR log error and exit process with a failure
+    console.error(error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 //Import EXPRESS
 const express = require("express");
 
+//Import logic to CONNECT to a DATABASE
+const connectDB = require("./config/db");
+
 //Initialize a main APP variable
 const app = express();
+
+//Create a CONNECTION to DATABASE
+connectDB();
 
 //Create a TEST ROUTE (for GET request to root folder '/')
 app.get("/", (req, res) => res.send("Hello from TEST ROUTE of our API!"));
