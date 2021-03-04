f3d05d0dfd49c23d5c143246049866722c5c3b54
Iurii Lopatenko <yalopatenko@gmail.com>
2021-03-04 14:39:16 -0800

    6.4 Clean Up and Initial Components. Now App returns a landing page with navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
+//Import React, Fragment
+import React, { Fragment } from 'react';
+
+//Import App.css
 import './App.css';
 
+//Import COMPONENTS (Navbar and Landing)
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+
+//Create a MAIN component App
 const App = () => {
   return (
-    <div>
-      <h3>hello</h3>
-    </div>
+    <Fragment>
+      <Navbar />
+      <Landing />
+    </Fragment>
   );
 };
 
