c67ccccd02004ccd3da3ad97da2c8dc4c71c0021
Iurii Lopatenko <yalopatenko@gmail.com>
2021-06-10 18:39:45 -0700

    All general routes were created

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express');
+
 const connectToDB = require('./config/db');
+
+const userRoute = require('./routes/api/user');
+const profileRoute = require('./routes/api/profile');
+const postRoute = require('./routes/api/post');
+const authRoute = require('./routes/api/auth');
 
 const app = express();
 connectToDB();
 
 app.get('/', (req, res) => res.send('API is running'));
+
+//define all the routes
+app.use('/api/user', userRoute);
+app.use('/api/profile', profileRoute);
+app.use('/api/post', postRoute);
+app.use('/api/auth', authRoute);
 
 const PORT = process.env.PORT || 5000;
 
