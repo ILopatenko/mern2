b488e877f4a99c05e51c85bccc7a32ed179d0fc8
Iurii Lopatenko <yalopatenko@gmail.com>
2021-06-25 11:59:45 -0700

    GET ALL THE PROFILES and GET A PROFILE BY USER ID functionality. 4.4

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -92,4 +92,39 @@ router.post(
     }
   }
 );
+
+//@route      GET api/profile
+//@desc       Get all the profiles
+//@access     Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server ERROR!');
+  }
+});
+
+//@route      GET api/profile/user/:userID
+//@desc       Get a profile by user ID
+//@access     Public
+router.get('/user/:userID', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.params.userID }).populate(
+      'user',
+      ['name', 'avatar']
+    );
+    if (!profile) {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    if (error.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server ERROR!');
+  }
+});
 module.exports = router;
